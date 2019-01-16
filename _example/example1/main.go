42c044e568b8a66a77a1aef93d1eda5fc2714b42
Ruotian <me@zrt.io>
2019-01-16 10:37:19 +0800

    example 缩小10%~100%

--- _example/example1/main.go
+++ _example/example1/main.go
@@ -17,13 +17,11 @@ func main() {
 	energyImg := SliceToImg(energyFunc(img, 1+2))
 	SavePNG(energyImg, "img_energy.png") // show energy img
 
-	CarvingX(img, energyFunc, true)
-	CarvingY(img, energyFunc, true) // show a seam example
-
 	for i := 0; i < 10; i++ {
-		newImg := Resize(img, energyFunc, 1280*(i+1)/10, 868)
-		fmt.Printf("%v\n", newImg.Bounds().Size())      // show newImg size
-		SavePNG(newImg, fmt.Sprintf("output%d.png", i)) // show newImg
+		newImg, newImgWithSeam := Resize(img, energyFunc, 1280*(i+1)/10, 868, nil)
+		fmt.Printf("%v\n", newImg.Bounds().Size())                   // show newImg size
+		SavePNG(newImg, fmt.Sprintf("output%d.png", i))              // show newImg
+		SavePNG(newImgWithSeam, fmt.Sprintf("output%d_seam.png", i)) // show newImg
 	}
 
 }
